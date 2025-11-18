9c892177958159c0274f1f50e4cef6128bfc66b2
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-19 01:14:28 +0200

    oo

--- Primes.java
+++ Primes.java
@@ -1,5 +1,16 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int a = Integer.parseInt(args[0]);
+        if (a==0 || a==1 || a<0) System.out.println(0);;
+            
+        System.out.println(2);
+        int c=0;
+        for (int i =3 ; i<a; i++){
+        for(int j =2 ; j<i; j++){
+         if (i%j!=0) c++;
+        }
+        if(c== i-2) System.out.println(i);
+        }
+
     }
 }
