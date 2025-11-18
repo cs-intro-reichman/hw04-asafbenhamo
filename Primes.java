c7cb4b6cac1f587240ef325f464754874d282300
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-19 01:44:00 +0200

    cc

--- Primes.java
+++ Primes.java
@@ -1,22 +1,27 @@
-public class Primes {
+public class Primes { 
+
+        public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true; 
+    }
+
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
-        int count = 1;
-        if (a==0 || a==1 || a<0) System.out.println(0);;
-        System.out.println( "Prime numbers up to " + a + ":");  
-        System.out.println(2);
-        int c=0;
-        for (int i =3 ; i<=a; i++){
-        for(int j =2 ; j<i; j++){
-         if (i%j!=0) c++;
-        }
-        if(c== i-2) {
-            System.out.println(i);
-            count++;
-        }
-        }
-
-        int p = a / count;
-        System.out.println("There are" + count + "primes between 2 and " +a+ "("+p+"% are primes)");
+        int count = 0;
+        if (a < 2) System.out.println(0);
+        System.out.println("Prime numbers up to " + a + ":");
+        for (int i = 2; i <= a; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+                count++;
+            }
+        double p= (double) count / a * 100.0;
+        System.out.println("There are " + count + " primes between 2 and " +a+ " ("+p+"% are primes)");
     }
+}
 }
