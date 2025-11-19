fa531939137901283cca48778e2e3652fe631cce
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-19 19:26:46 +0200

    sss

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -1,3 +1,5 @@
+import javax.print.DocFlavor.STRING;
+
 public class KeywordsDetector {
     public static void main(String[] args) {
         String[] sentences = {
@@ -17,10 +19,37 @@ public class KeywordsDetector {
         String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
         detectAndPrint(sentences, keywords);
     }
+public static String [] changetoarr(String s) {
+        // Replace this comment with your code
+        String [] arr = new String[s.length()];
+        String h = "";
+        int j=0;
+        for(int i =0; i<s.length() ; i++){
+               if (s.charAt(i) != ' ') h = h + s.charAt(i);
+               else {
+                arr[j] = h;
+                j++;
+                h="";
+               }
+            }
+            return arr;
+        }
+    
 
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         // Replace this comment with your code
+        for(int i =0; i<sentences.length ; i++){
+         String [] arr = new String[sentences[i].length()];
+          arr = changetoarr(sentences[i]);
+          for(int v =0; v<arr.length;v++){
+            int m =0;
+                for (int c= 0 ; c< keywords.length; c++){
+               if ( arr[v] ==  keywords[c]) m++;
+             }
+             if (m>0) System.out.println(sentences[i]);
+         }
+        }
     }
 }
